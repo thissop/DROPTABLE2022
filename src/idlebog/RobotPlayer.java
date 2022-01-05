1d68882249a746f8742c0484a12ca223615fe1c7
Jay Jayjay <jayjay.jay20162016@gmail.com>
2022-01-04 20:50:59 -0500

    initial

---
+++ src/idlebog/RobotPlayer.java
@@ -0,0 +1,13 @@
+package idlebog;
+
+import battlecode.common.*;
+import java.util.Random;
+
+public strictfp class RobotPlayer {
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+                Clock.yield();
+        }
+    }
+}
